ecf6b26def5daf28c21ecd6939c4ac4a17a55e6f
jigi <micekj@naver.com>
2025-12-20 23:51:33 +0900

    fix : manager
    gameflowmanager 추가, 기타 매니저 수정

--- Idol_debut_project/Assets/Scripts/Managers/GameManager.cs
+++ Idol_debut_project/Assets/Scripts/Managers/GameManager.cs
@@ -7,12 +7,12 @@ public class GameManager : MonoBehaviour
     // GameStateMachie을 통해 상태 전환 제어
     // 각 State의 Enter, Update, Exit 호출 보장
     // 다른 시스템들이 현재 게임 상태를 조회할 수 있는 전역 진입점 제공
-    
+
     // 책임 범위
     // 게임 시작 시 초기 State 설정
     // 상태 전환 요청을 받아 StateMachine에 전달
     // 전역 싱글톤 인스턴스 제공
-    
+
     // 하지 말것
     // Player 스탯 직접 수정
     // UI 로직 직접 처리
@@ -20,6 +20,10 @@ public class GameManager : MonoBehaviour
     // 개별 GameState 내부 로직 관여
     public static GameManager Instance;
     private GameStateMachine _stateMachine;
+
+    [SerializeField] private Player player;
+    [SerializeField] private TimeCycleManager time;
+    [SerializeField] private GameFlowManager gameFlow;
 
     void Awake()
     {
@@ -29,7 +33,9 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-        _stateMachine.ChangeState(new Rest());
+        _stateMachine.ChangeState(
+         new Rest(player, time, gameFlow)
+     );
     }
 
     void Update()
--- Idol_debut_project/Assets/Scripts/Managers/TimeCycleManager.cs
+++ Idol_debut_project/Assets/Scripts/Managers/TimeCycleManager.cs
@@ -4,6 +4,7 @@ public class TimeCycleManager : MonoBehaviour
 {
     public int currentMonth = 1;
     public int currentWeek = 1;
+    public bool didComeBack;
 
     public GameStateMachine gameStateMachine;
     public Player player;
@@ -11,11 +12,30 @@ public class TimeCycleManager : MonoBehaviour
     public void AdvanceWeek()
     {
         currentWeek++;
+
+        if(currentWeek > 4)
+        {
+            EndMonth();
+        }
     }
 
     public void EndMonth()
     {
         currentWeek = 1;
         currentMonth++;
+        didComeBack = false;
+    }
+
+    //연속 평판
+    public int repeatedNegative = 0;
+
+    public void ResetNegativeReputation()
+    {
+        repeatedNegative = 0;
+    }
+
+    public void IncreaseNegativeReputation()
+    {
+        repeatedNegative++;
     }
 }
