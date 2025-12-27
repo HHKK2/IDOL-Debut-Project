46ebdc5e8e0f99a270bc40e943341ec6e249a04d
jigi <micekj@naver.com>
2025-12-27 21:00:10 +0900

    add : homescenecontroller

---
+++ Idol_debut_project/Assets/Scripts/SceneController/HomeSceneController.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class HomeSceneController : MonoBehaviour
+{
+    private HomeHUD homeHUD;
+
+    void Start()
+    {
+        // HUD 찾기
+        homeHUD = UIManager.Instance.HUDList.Find(h => h is HomeHUD) as HomeHUD;
+
+        if (homeHUD == null)
+            homeHUD = UIManager.Instance.ShowHUDUI<HomeHUD>();
+
+        // 버튼 이벤트 연결
+        homeHUD.ClickedNewGameButton += OnNewGame;
+        homeHUD.ClickedLoadButton += OnLoad;
+        homeHUD.ClickedExitButton += OnExit;
+    }
+
+    private void OnDestroy()
+    {
+        if (homeHUD == null) return;
+
+        homeHUD.ClickedNewGameButton -= OnNewGame;
+        homeHUD.ClickedLoadButton -= OnLoad;
+        homeHUD.ClickedExitButton -= OnExit;
+    }
+
+    private void OnNewGame()
+    {
+        // 튜토리얼 시작
+        GameSceneManager.Instance.ChangeScene(GameScenes.TutorialScene);
+    }
+
+    private void OnLoad()
+    {
+        //TODO
+        Debug.Log("Load Game (나중에 구현)");
+    }
+
+    private void OnExit()
+    {
+        Application.Quit();
+    }
+}
