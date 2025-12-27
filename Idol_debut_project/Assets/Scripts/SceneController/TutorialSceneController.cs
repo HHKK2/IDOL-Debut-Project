164061a2c68d0dc951d31bfa16d5366451271f95
jigi <micekj@naver.com>
2025-12-28 01:58:40 +0900

    fix : 주석수정

--- Idol_debut_project/Assets/Scripts/SceneController/TutorialSceneController.cs
+++ Idol_debut_project/Assets/Scripts/SceneController/TutorialSceneController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections;
 
 /// <summary>
-/// 임시로 튜토리얼을 넘길 방법이 필요해서 대충 짜놓음... 다 삭제하고 다시 짜는 걸 ㅊㅊ
+/// 임시로 튜토리얼을 넘길 방법이 필요해서 대충 짜놓음...
 /// tutorialScene에 있는 버튼도 삭제하십쇼
 /// 테스트용으로 튜토리얼 씬에서 버튼을 누르면 homescene으로 넘어가게 해놓음
 /// </summary>
@@ -11,7 +11,6 @@ public class TutorialSceneController : MonoBehaviour
 {
     public void OnClickFinishTutorial()
     {
-        GameManager.Instance.StartGame();
         GameSceneManager.Instance.ChangeScene(GameScenes.HomeScene);
     }
 
